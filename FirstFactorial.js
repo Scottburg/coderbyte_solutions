0365706bf1678608884053705d8ac8bae4026961
Scottburg <scottb4545@gmail.com>
2020-08-16 09:31:34 +0100

    Solution to FirstFactorial

---
+++ FirstFactorial.js
@@ -0,0 +1,13 @@
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it.
+// For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases,
+//  the range will be between 1 and 18 and the input will always be an integer.
+
+function FirstFactorial(num) {
+  let sum = num;
+
+  for (let i = 1; i < num; i++) {
+    sum = sum * (num - i);
+  }
+
+  return sum;
+}
