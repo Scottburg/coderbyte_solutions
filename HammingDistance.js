be24d4ebf7f927bea885258d34c9d6ab4828e848
Scottburg <scottb4545@gmail.com>
2020-08-16 12:51:37 +0100

    Solution to HammingDistance

---
+++ HammingDistance.js
@@ -0,0 +1,16 @@
+// Have the function HammingDistance(strArr) take the array of strings stored in strArr,
+// which will only contain two strings of equal length and return the Hamming distance
+// between them. The Hamming distance is the number of positions where the corresponding
+// characters are different. For example: if strArr is ["coder", "codec"] then your program
+// should return 1. The string will always be of equal length and will only contain lowercase
+// characters from the alphabet and numbers.
+
+function HammingDistance(strArr) {
+  let res = 0;
+  for (let i = 0; i < strArr[0].length; i++) {
+    if (strArr[0][i] !== strArr[1][i]) {
+      res++;
+    }
+  }
+  return res;
+}
