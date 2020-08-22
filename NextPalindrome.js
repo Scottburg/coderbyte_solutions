ed43282ebe2929dffa7d6ad7a4bfda87ccd9eb8d
Scottburg <Scottb4545@gmail.com>
2020-08-22 12:05:37 +0100

    Solution to NextPalindrome

---
+++ NextPalindrome.js
@@ -0,0 +1,17 @@
+// Have the function NextPalindrome(num) take the num parameter being passed and return the next largest
+// palindromic number. The input can be any positive integer. For example: if num is 24, then your program
+// should return 33 because that is the next largest number that is a palindrome.
+
+function NextPalindrome(num) {
+  while (num <= num * num) {
+    num++;
+    if (num.toString() === num.toString().split('').reverse().join('')) {
+      return num;
+    }
+  }
+}
+
+// keep this function call here
+console.log(NextPalindrome(504)); //505
+console.log(NextPalindrome(490)); //494
+console.log(NextPalindrome(1)); //494
