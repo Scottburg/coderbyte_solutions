b87a76a5f8ea95b30b407a624afd8f323abb24d7
Scottburg <Scottb4545@gmail.com>
2020-08-23 10:14:06 +0100

    Solution to ClosestEnemy

---
+++ ClosestEnemy.js
@@ -0,0 +1,24 @@
+// Have the function ClosestEnemy(arr) take the array of numbers stored in arr and from the position
+// in the array where a 1 is, return the number of spaces either left or right you must move to reach
+// an enemy which is represented by a 2. For example: if arr is [0, 0, 1, 0, 0, 2, 0, 2] then your program
+// should return 3 because the closest enemy (2) is 3 spaces away from the 1. The array will contain any
+// number of 0's and 2's, but only a single 1. It may not contain any 2's at all as well, where in that
+// case your program should return a 0.
+
+function ClosestEnemy(arr) {
+  let res = Infinity;
+  const loc = arr.findIndex((el) => el === 1);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 2) {
+      let diff = i > loc ? i - loc : loc - i;
+      if (diff < res) {
+        res = diff;
+      }
+    }
+  }
+
+  return res === Infinity ? 0 : res;
+}
+
+// keep this function call here
+console.log(ClosestEnemy([0, 0, 1, 0, 0, 2, 0, 2])); //3
