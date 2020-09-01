7b7089de756193abfc62693c58b5543ef0bb9b43
scottburg <scottb4545@gmail.com>
2020-09-01 12:33:18 +0100

    Solution to MultipleBrackets

---
+++ MultipleBrackets.js
@@ -0,0 +1,47 @@
+// Have the function MultipleBrackets(str) take the str parameter being passed and return 1 #ofBrackets if the brackets are
+// correctly matched and each one is accounted for. Otherwise return 0. For example: if str is "(hello [world])(!)", then the
+// output should be 1 3 because all the brackets are matched and there are 3 pairs of brackets, but if str is "((hello [world])"
+// the the output should be 0 because the brackets do not correctly match up. Only "(", ")", "[", and "]" will be used as brackets.
+// If str contains no brackets return 1.
+
+function MultipleBrackets(str) {
+  // code goes here
+  // no brackets return 1
+  // three types ( { [
+  // make an array of the brackets add for opening { and pop off
+  // if length of all at end === 0 then return 1 else 0
+  let curly = 0;
+  let square = 0;
+  let round = 0;
+  let total = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(') {
+      round++;
+      total++;
+    }
+    if (str[i] === ')') {
+      round--;
+    }
+    if (str[i] === '{') {
+      curly++;
+      total++;
+    }
+    if (str[i] === '}') {
+      curly--;
+    }
+    if (str[i] === '[') {
+      square++;
+      total++;
+    }
+    if (str[i] === ']') {
+      square--;
+    }
+  }
+
+  return curly + square + round === 0 ? `1 ${total}` : 0;
+}
+
+// keep this function call here
+console.log(MultipleBrackets('(coder)[byte)]')); // 0
+console.log(MultipleBrackets('(c([od]er)) b(yt[e])')); // 1 5
